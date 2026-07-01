ea5062b3c36984f169bdd18d84b63a13b04d501f
Gautam <luckygautam430@gmail.com>
2026-07-01 18:17:15 +0530

    Fixed CORS issue for Render and Vercel

--- Backend/server.js
+++ Backend/server.js
@@ -14,7 +14,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://gupshup-chat-psi.vercel.app"],
+  }),
+);
 app.use(express.json());
 
 // Routes Middleware
@@ -35,7 +39,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:5173", // Default Vite+React port for frontend
+    origin: ["http://localhost:5173", "https://gupshup-chat-psi.vercel.app"], // Default Vite+React port for frontend
     methods: ["GET", "POST"],
   },
 });
