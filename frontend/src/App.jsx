1855a9b5ad5feb11fceb50add91f112453991867
Gautam <luckygautam430@gmail.com>
2026-08-17 20:03:04 +0530

    Added Route Protection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,16 +10,29 @@ import ChatPage from "./pages/ChatPage";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
+  // Check if user is logged in by looking for data in localStorage
+  const user = JSON.parse(localStorage.getItem("userInfo"));
+
   return (
     <>
       <SpeedInsights />
       <Router>
         <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+          {/* Redirect to chats if the user is already logged in */}
+          <Route
+            path="/login"
+            element={user ? <Navigate to="/chats" /> : <Login />}
+          />
+          <Route
+            path="/signup"
+            element={user ? <Navigate to="/chats" /> : <Signup />}
+          />
 
-          {/* Protected Chat Route */}
-          <Route path="/chats" element={<ChatPage />} />
+          {/* Protected Chat Route: Redirect to login if NOT logged in */}
+          <Route
+            path="/chats"
+            element={user ? <ChatPage /> : <Navigate to="/login" />}
+          />
 
           {/* Default route redirects to login */}
           <Route path="*" element={<Navigate to="/login" />} />
